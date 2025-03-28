c375b16c22448efa3948855466a910655b4ba16a
Tomas Martinkus <tomreikalai111@gmail.com>
2025-03-28 15:45:40 +0200

    frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+//import Header from './components/Header';
+//import Login from './components/Login';
+//import QuestionForm from './components/QuestionForm';
+//import MovieRecommendation from './components/MovieRecommendation';
+//import WatchList from './components/WatchList';
+
+function App() {
+  // return (
+  //   <div>
+  //     <Header />
+  //     <Login />
+  //     <QuestionForm />
+  //     <MovieRecommendation />
+  //     <WatchList />
+  //   </div>
+  // );
+}
+
+export default App;
